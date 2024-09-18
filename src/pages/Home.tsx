2f34b7ef8aa73f891dec34ee3eb1e3abd8571034
JUAN PABLO RESTREPO JIMENEZ <jprestrepo346@gmail.com>
2024-09-18 12:51:09 -0500

    v2: changed styles of home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,10 +5,10 @@ import '../styles/home.css'
 export default function Home() {
     return (
         <View>
-            <div className='rick-morty-main-container'>
-                <div className='rick-morty-header'>
-                <h1 className='rick-morty-title'>Rick and Morty</h1>
-                <div className="rick-morty-link-icon">
+            <div className='main-container'>
+                <div className='header'>
+                <h1 className='title'>Rick and Morty</h1>
+                <div className="link-icon">
                 <p>Icon from:</p>
                 <a className='rick-morty-link' target="_blank" href="https://icons8.com">Icons8</a>
                 <a className='rick-morty-link' target="_blank" href="https://icons8.com/icon/6NdESBVdyMv8/rick-sanchez">Rick Sanchez</a>
